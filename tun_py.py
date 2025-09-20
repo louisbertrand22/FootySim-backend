5c2bc14730baf3240038017c66fd7be476fc46eb
louis.bertrand <louis.bertrand@epita.fr>
2025-09-21 00:08:53 +0200

    fix requirement and docker

--- tun_py.py
+++ tun_py.py
@@ -1,16 +1,4 @@
 import asyncio
-from dotenv import load_dotenv
-from footysim.db.session import AsyncSessionLocal
-from footysim.seeds.seed_data import seed_minimal
-
-# 👇 force l’import de tous les modèles pour que les relations soient connues
-from footysim.models import club, country, fixture, goal, league, match, player, season, stadium, transfer
-
-load_dotenv()
-
-async def run():
-    async with AsyncSessionLocal() as s:
-        await seed_minimal(s)
-
-asyncio.run(run())
-print("✅ Données seed insérées.")
+from footysim.db.session import init_models
+asyncio.run(init_models())
+print("✅ tables créées")
